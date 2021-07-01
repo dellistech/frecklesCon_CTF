76d8f8d4e35671f13d590e0e715361c0a6eca5b3
dellistech <dev@dellistech.com>
2021-06-30 18:38:30 -0600

    Inital commit for babysFirst pwn

---
+++ pwn/babysFirst/first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void function(int a, int b, int c) {
+    char buffer1[5];
+    char buffer2[10];
+} 
+
+int main() {
+    printf("hello, world! \n");
+    function(1,2,3);
+    return 0;
+}
