67b7a4c08c57a9bae321f9909a649f1a52ef2a49
Expert_Cactus <fake.markus@gmail.com>
2021-08-04 21:29:33 -0400

    hopefully some fun challenges, second is more fleshed out than first

---
+++ reversing/first-re.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(int argc, char** argv) 
+{
+    //baby reversing challenge, xor of flag
+    unsigned char xor_key = 0x42;
+
+    //char* for obfuscated key
+}
---
+++ reversing/second-re.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+
+int main(void)
+{
+    struct addrinfo* result = NULL;
+    
+    int error;
+
+    printf("I'll try to reach out to the internet for the key!\n");
+
+    error = getaddrinfo("base64obfuscatedkeygoeshere",NULL,NULL,&result);
+
+    if (error != 0)
+    {
+        printf("error, the key was not found, maybe you can take a look and see where I went wrong.\n");
+    }else {
+        printf("something went wrong, that wasn't supposed to resolve, take a look\n");
+    }
+
+    if (NULL == result) 
+    {
+        printf("result is null\n");
+    }else
+    {
+        freeaddrinfo(result);
+    }
+    
+    return 0;
+}
